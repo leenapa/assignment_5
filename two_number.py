088afa5c60a498f8209ae0184aa174c3a90dc37e
leenapa <108974779+leenapa@users.noreply.github.com>
2022-08-15 09:58:20 +0530

    Add files via upload

---
+++ two_number.py
@@ -0,0 +1,13 @@
+from curses import A_ALTCHARSET
+from re import A
+
+
+class Mathh:
+
+    def __init__(self):
+        pass
+    def pow(self, x, n):
+        a=x**n
+        return a
+leena=Mathh()
+leena.pow(10,2)
